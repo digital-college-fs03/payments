fabcf7da6b5eefb666875beab56733f667044d41
William Correa <wilcorrea@gmail.com>
2024-04-04 21:38:31 -0300

    feature: criar controller de transferencia

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 import express from 'express'
 const app = express()
 const port = 3000
-import database from './database.js'
+import transferencia from './controllers/transferencia.js'
 
 // for parsing application/json
 app.use(express.json())
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/transaction', async function (request, response) {
-  /**
-   * insert into transferencias
-   * (id_usuario_origem, id_usuario_destino, valor)
-   * values
-   * (4, 15, 100)
-   */
-    const data = await database
-      .insert({
-        id_usuario_origem: request.body.payer,
-        id_usuario_destino: request.body.payee,
-        valor: request.body.value
-      })
-      .into('transferencias')
-    response.send(data)
-})
+// clean code
+app.post('/transaction', transferencia)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
